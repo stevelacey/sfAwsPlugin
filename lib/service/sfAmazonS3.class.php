ed3e0f86b1463dae1560a2df6d772baac88c0398
Steve Lacey <steve.lacey@wiredmedia.co.uk>
2011-10-04 13:02:43 +0100

    Parse method arguments and pass bucket from config where applicable.

--- lib/service/sfAmazonS3.class.php
+++ lib/service/sfAmazonS3.class.php
@@ -10,17 +10,22 @@
  */
 class sfAmazonS3 {
   private $S3;
-  
+
   public function __construct() {
     $this->S3 = new AmazonS3();
   }
   
   public function __call($name, $arguments) {
-    /* rudementary, more methods accept bucket as first param, change to use reflection */
-    if (strstr($name, 'createObject') || strstr($name, 'getObject')) {
-      array_unshift($arguments, sfConfig::get('app_aws_bucket'));
+    $method = sfInflector::underscore($name);
+
+    $reflection = new ReflectionMethod(get_class($this->S3), $method);
+
+    if (count($arguments) < $reflection->getNumberOfParameters()) {
+      if (current($reflection->getParameters())->name == 'bucket') {
+        array_unshift($arguments, sfConfig::get('app_aws_bucket'));
+      }
     }
 
-    return call_user_func_array(array($this->S3, sfInflector::underscore($name)), $arguments);
+    return call_user_func_array(array($this->S3, $method), $arguments);
   }
 }
